b4cd3fb488ea9fb5adc0ff27afe2fca90a3c098a
Christophe Beveraggi <beve@croll.fr>
2016-10-25 01:04:07 +0200

    Event list templates done

--- src/pages/event-list/event-list.ts
+++ src/pages/event-list/event-list.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { EventFormPage } from '../event-form/event-form';
 
-/*
-  Generated class for the EventList page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-event-list',
   templateUrl: 'event-list.html'
 })
+
 export class EventListPage {
+
+  eventList: any;
 
   constructor(public navCtrl: NavController) {}
 
   ionViewDidLoad() {
-    console.log('Hello EventList Page');
+
+    this.eventList = [
+      {name: "Premier event de test", description: "La description de l'event", start_date: new Date(), end_date: new Date()},
+      {name: "Deuxième event de test", description: "La description de l'event", start_date: new Date(), end_date: new Date()},
+      {name: "Troisième event de test", description: "La description de l'event", start_date: new Date(), end_date: new Date()},
+    ];
+  }
+
+  add() {
+    console.log("ADD !");
+    this.navCtrl.push(EventFormPage);
   }
 
 }
