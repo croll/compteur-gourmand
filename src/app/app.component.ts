9b9a91c14da5c11a80dca058f9c7c2b2e01d8384
Nicolas Dimitrijevic <nicolas@croll.fr>
2016-10-25 09:53:47 +0200

    load miracast on boot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,33 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 import { HomePage } from '../pages/home/home';
+import { Presentation } from '../pages/presentation/presentation';
+import { CgMiracast } from '../providers/cg-miracast';
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class CompteurGourmand {
-  rootPage = HomePage;
+  //rootPage = HomePage;
+  rootPage: any
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, public miracast: CgMiracast) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+
+    switch (miracast.mode) {
+        case "tablette":
+          this.rootPage = HomePage;
+        break;
+        case "presentation":
+          this.rootPage = Presentation;
+        break;
+        default:
+          console.error("Pas de cg-part !");
+          this.rootPage = HomePage;
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,42 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { CompteurGourmand } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Presentation } from '../pages/presentation/presentation';
 import { FootprintPage } from '../pages/footprint/footprint';
 import { ChoosePage } from '../pages/choose/choose';
 import { CommitmentFormPage } from '../pages/commitment-form/commitment-form';
 import { AdminPage } from '../pages/admin/admin';
 import { TinyMCE } from './tinymce.directive';
 import { Database } from './database.service';
+import { CgMiracast } from '../providers/cg-miracast';
 
 @NgModule({
   declarations: [
     CompteurGourmand,
     CommitmentFormPage,
     HomePage,
+    Presentation,
     FootprintPage,
     ChoosePage,
     AdminPage,
     TinyMCE
   ],
   imports: [
-    IonicModule.forRoot(CompteurGourmand)
+      IonicModule.forRoot(CompteurGourmand)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     CompteurGourmand,
     CommitmentFormPage,
     HomePage,
+    Presentation,
     FootprintPage,
     AdminPage,
     ChoosePage
   ],
   providers: [
-    Database
+    Database,
+    CgMiracast,
   ]
 })
 export class AppModule {}
