d977e6f9a5e819e01555dc567e8f466354b766f2
Nicolas Dimitrijevic <nicolas@croll.fr>
2016-10-26 20:04:43 +0200

    event_commitment db model

--- src/db/event_commitment.ts
+++ src/db/event_commitment.ts
@@ -1,14 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Database } from '../app/database.service';
+import { Store } from './store';
+
 import docuri from 'docuri';
 
 export class Event_Commitment {
-  private static route = docuri.route("event_commitment/:id_event/:id_commitment");
-
   _id: string
   id_event: string
   id_commitment: string
 
-  make_id() {
-    this._id = Event_Commitment.route(this);
+  constructor(value = null) {
+    if (value != null) {
+      Object.assign(this, value);
+    }
   }
 
+}
+
+@Injectable()
+export class StoredEvent_Commitment extends Store {
+  constructor(db: Database) {
+    super(db, docuri.route("event_commitment/:id_event/:id_commitment"), "event_commitment/", "event_commitment0");
+  }
+
+  put(doc: Event) {
+    return super.put(doc);
+  }
 }
